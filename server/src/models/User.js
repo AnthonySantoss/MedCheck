c2b626e3d240c6fe2ac202f7c5486ddcaed0c98b
AnthonySantoss <16034688@sempreuninassau.com.br>
2025-09-15 21:20:53 -0300

    Refatoração do user.js

--- server/src/models/User.js
+++ server/src/models/User.js
@@ -1,7 +1,30 @@
-//
-const user = mongoose.model('user', {
-    id: Number,
-    name: String,
-    email: String,
-    senha: String
-})
+//Importando o mongoose
+const mongoose = require(mongoose);
+
+// Criando esquema de usuário
+const userSchema = new mongoose.Schema({
+    nome: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase:true
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now()
+    }
+});
+
+//Criação de modelo a partir do esquema
+const User = mongoose.model('User', userSchema);
+
+//Exporta o modelo
+module.exports = User;
